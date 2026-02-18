aa3018478da3c94fbac04abccf0e9b627bfb6fde
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-19 00:32:19 +0530

    added a driverfile

---
+++ src/assignment12/driver.py
@@ -0,0 +1,11 @@
+from util import min_max
+if __name__ == '__main__':
+
+ a, b = map(int, input().split())
+ values = []
+ for i in range (a):
+     a = list(map(int, input().split()))
+     values.append(a)
+
+result = min_max(matrix)
+print(result)
--- src/assignment12/util.py
+++ src/assignment12/util.py
@@ -1,5 +1,5 @@
 import numpy as np
-def min_max(matrix):
-    x = np.array(matrix, datatype = int)
+def min_max(values):
+    x = np.array(values, datatype = int)
     min_axis = np.min(x, axis = 1)
     return np.max(min_axis)
