fd0a3c2ff9fcf8c468baa02bf560adf8d8cb987a
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-18 20:49:34 +0530

    added test case for assignment3

---
+++ src/assignment3/test_file.py
@@ -0,0 +1,6 @@
+from util import second_maximum
+def test_questionarray():
+    assert second_maximum([2, 3, 6, 6, 5]) == 5
+def test_case1():
+    assert second_maximum([10, 50, 68, 42, 39]) == 50
+
