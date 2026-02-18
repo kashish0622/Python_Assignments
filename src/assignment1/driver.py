ec37841dcea746783693173ef533234be8d9bbf6
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-18 18:16:32 +0530

    added driver.py for assignment1

---
+++ src/assignment1/driver.py
@@ -0,0 +1,13 @@
+from util import avg
+
+n = int(input())
+student_marks = {}
+
+for i in range(n):
+    data = input().split()
+    name = data[0]
+    marks = list(map(float, data[1:]))
+    student_marks[name] = marks
+
+query_name = input()
+average = avg(student_marks, query_name)
