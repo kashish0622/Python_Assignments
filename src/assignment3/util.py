b84ba277faaa7b752b869d11dfd3af08ea1c3d91
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-18 21:01:57 +0530

    run pytest to test the testcases

--- src/assignment3/util.py
+++ src/assignment3/util.py
@@ -1,5 +1,5 @@
 def second_maximum(score):
     single_value = list(set(score))
     single_value.sort()
-    runner_up = (score(score.index(max(score)))-1)
+    runner_up = single_value[-2]
     return runner_up
