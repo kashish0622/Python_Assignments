05647f2cae4829bdccd716e888265d9b0d84e317
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-18 19:38:14 +0530

    added a test case in  testfile

---
+++ src/assignment1/testfile.py
@@ -0,0 +1,12 @@
+from util import runcommands
+def test_commands():
+    commands = [
+        'append 5',
+        'print 5',
+        'remove 5',
+        'insert 7, 9',
+        'sort',
+        'pop(7)',
+        'reverse'
+
+    ]
