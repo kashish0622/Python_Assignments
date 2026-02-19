9a62726e7d55c68eb7a299bcfb40406c08fc9e71
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-19 11:29:50 +0530

    added a driver file for assignment19

---
+++ src/assignment19/driver.py
@@ -0,0 +1,6 @@
+from util import is_valid
+n = int(input("Enter number of emails: "))
+emails = [input().strip() for _ in range(n)]
+valid_emails = sorted(filter(is_valid, emails))
+print(valid_emails)
+if __name__ == '__main__':
